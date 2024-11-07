c783ca1e8829559c1cd0624f37edf733eb7a209b
Rifqim <rifqimuh390@gmail.com>
2024-11-07 21:54:59 +0700

    update section product

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,21 +6,23 @@ function Home() {
     <>
       <div
         id="home"
-        className="w-full h-[90%] bg-orange-white flex flex-col md:flex-row justify-center scroll-m-20"
+        className="flex flex-col md:flex-row justify-center scroll-m-20"
       >
         <div className=" md:pl-20 pl-3 md:pt-16 pt-3">
           <h1 className="md:text-5xl text-3xl font-bold">
             Buat dokumen pekerjaanmu dengan lebih cepat dan praktis!
           </h1>
           <p className="md:text-xl text-base pt-5 md:pt-10">
-            Permudah pekerjaanmu dengan DokumenKerjaku. Buat CV dan surat
-            lamaran hanya dengan sekali klik!
+            Permudah pekerjaanmu dengan DokumenKerjaku. Buat CV dan dokumen lain
+            hanya dengan sekali klik!
           </p>
-          <button className="flex items-center bg-orange-600 md:mt-5 mt-4 md:py-2 py-1 md:px-5 px-4 rounded-full text-white hover:bg-opacity-95">
-            <RxFilePlus className=" size-8" />
-            <p className="md:text-xl text-base font-bold">
-              Buat Dokumen Sekarang
-            </p>
+          <button className=" bg-orange-600 md:mt-5 mt-4 md:py-2 py-1 md:px-5 px-4 rounded-full text-white hover:bg-opacity-95">
+            <a href="#product" className="flex items-center">
+              <RxFilePlus className=" size-8" />
+              <p className="md:text-xl text-base font-bold">
+                Buat Dokumen Sekarang
+              </p>
+            </a>
           </button>
         </div>
         <img
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -24,17 +24,21 @@ function Navbar() {
 
   return (
     <>
-      <header className="sticky top-0 w-full bg-orange-600">
+      <header
+        className={`sticky top-0 w-full bg-orange-600 ${
+          scroll ? "shadow-md" : ""
+        }`}
+      >
         <nav
           className={`flex items-center justify-between text-white md:w-[90%] mx-auto p-2 duration-500 ${
-            scroll ? "md:p-3 shadow-md" : "md:p-4 shadow-none"
+            scroll ? "md:p-3" : "md:p-4"
           }`}
         >
           <a href="/">
             <img src={logo} alt="logo" className="md:w-[180px] w-[150px]" />
           </a>
           <ul
-            className={`md:static absolute sm:text-xl text-lg flex flex-col md:flex-row md:gap-20 md:top-auto md:left-auto left-0 top-[58px] md:top-auto bg-orange-600 w-full md:w-auto  h-[100dvh] md:h-auto md:duration-0 duration-500 ${
+            className={`md:overflow-x-visible overflow-x-hidden md:static absolute sm:text-xl text-lg flex flex-col md:flex-row md:gap-20 md:top-auto md:left-auto left-0 top-[58px] md:top-auto bg-orange-600 w-full md:w-auto  h-[100dvh] md:h-auto md:duration-0 duration-500 ${
               !navActive && "translate-x-full md:translate-x-0"
             }`}
           >
