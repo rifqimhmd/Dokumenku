6cce0044c8bfdfb158608318cfeb7790f652eb32
Rifqim <rifqimuh390@gmail.com>
2024-11-08 15:06:10 +0700

    update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
 
   return (
     <>
-      <div className="text-center md:p-5 p-3 border font-bold ">
+      <div className="text-center md:p-5 p-3 border md:font-bold ">
         <p>©{getYear()} DokumenKerjaku. All rights reserved.</p>
       </div>
     </>
